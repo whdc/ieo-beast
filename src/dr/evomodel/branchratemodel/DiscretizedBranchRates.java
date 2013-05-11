819a9b93707ace3e0e14d5edb85ccde997a34149
whdc <wchang@berkeley.edu>
2013-05-10 23:23:20 -0700

    Smaller rate for branch leading up to an ancestor
    When using restriction site characters, ucld.mean is on the order of 1e-5.
    I therefore reduced the rate on the branch leading up to an ancestor
    from 1 to 1e-5.

--- src/dr/evomodel/branchratemodel/DiscretizedBranchRates.java
+++ src/dr/evomodel/branchratemodel/DiscretizedBranchRates.java
@@ -193,7 +193,7 @@ public class DiscretizedBranchRates extends AbstractBranchRateModel {
         // cause the tree likelihood to return NaN.
         Taxon taxon = tree.getNodeTaxon( node);
         if( taxon != null && taxon.getAdventitious()) {
-          return 1.;
+          return 0.00001;
         }
 
         //System.out.println(rates[rateCategory] + "\t"  + rateCategory);
